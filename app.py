4d8b57e4d47841cdb79073b9584914767e146271
tringuyennn123-tech <tringuyennn123@gmail.com>
2025-08-30 11:42:21 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -95,7 +95,7 @@ def login():
     conn = get_db_connection()
     cur = conn.cursor()
     cur.execute(
-        "SELECT id FROM users WHERE username = %s AND password = %s",
+        "SELECT id, username FROM users WHERE username = %s AND password = %s",
         (username, password)
     )
     user = cur.fetchone()
